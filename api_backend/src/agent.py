53b4b234e14a75f10e99100f985ec8bb27ce9b13
Francesc <cesc.dovi@gmail.com>
2025-10-31 15:03:19 +0100

    Agent implemented with the new API

--- api_backend/src/agent.py
+++ api_backend/src/agent.py
@@ -1,12 +1,23 @@
-#from langchain_redis import RedisChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
+# from langchain.chains import RetrievalQA
+# from langchain_core.tools import tool
+# from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+# from langgraph.prebuilt import create_react_agent
+# from langchain_neo4j import Neo4jVector
+# from langchain_tavily import TavilySearch
+# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+# from langchain.chains import RetrievalQA
+# from langchain.agents import create_react_agent, AgentExecutor
+# from langchain.tools import tool
+# from langchain.chat_models import ChatOpenAI
+# from langchain.embeddings.openai import OpenAIEmbeddings
+
+from langchain_neo4j import Neo4jVector, GraphCypherQAChain, Neo4jGraph
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.agents import create_agent 
+from langchain.tools import tool
+from langchain_tavily import TavilySearch
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-#from langchain_tavily import TavilySearch
-from langchain_core.tools import tool
-from langchain_neo4j import Neo4jVector
-from langchain.chains import RetrievalQA
 
 from config.common_settings import settings
 from api_backend.src.agent_prompt import SYSTEM_AGENT_PROMPT, HUMAN_AGENT_PROMPT
@@ -32,51 +43,90 @@ vectorstore = Neo4jVector.from_existing_graph(
     database=settings.DATABASE_NEO4J_NAME,
     index_name="emb_index",
     embedding=embeddings_function,
-    node_label="CommonLabel",
-    text_node_properties=["nombre"],
+    node_label="Procedimiento",
+    text_node_properties=["nombre", "content"],
     embedding_node_property="embedding",
 )
 
 #create retriever from vector store
-retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-# create QA chain
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever,
-    chain_type="stuff",
-    verbose=True,
-    return_source_documents=True,
-)
+# # create QA chain
+# qa = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     retriever=retriever,
+#     chain_type="stuff",
+#     verbose=True,
+#     return_source_documents=True,
+# )
 
 
 ####### TOOL 1: query Neo4j DB #######
 @tool
 def neo4j_query(query: str) -> str:
     """Realiza una búsqueda semántica en la base de datos Neo4j usando embeddings para encontrar nodos relevantes y genera una respuesta contextual basada en esos datos."""
-    result = qa.invoke({"query": query})
+    result = retriever.invoke(query)
     logging.info(f"Neo4j query tool invoked with query: {query}")
-    return {
-        "result_keys": result.keys(),
-        "result": result["result"],
-        "source_documents": [doc.page_content for doc in result["source_documents"]],
-    }
+    return "\n\n".join(
+        f"Source: {doc.metadata}\nContent: {doc.page_content}" 
+        for doc in result
+    )
 
-tools = [neo4j_query]
 
-AGENT_PROMPT = ChatPromptTemplate.from_messages([
-    ("system", SYSTEM_AGENT_PROMPT),
-    ("human", HUMAN_AGENT_PROMPT),
-    MessagesPlaceholder(variable_name="agent_scratchpad"), #marks a spot in the prompt template where the agent inserts actions history
-])
+####### TOOL 2: query Neo4j DB #######
+#search_tool = TavilySearch(max_results=3, api_key=settings.TAVILY_API_KEY)
+
+tavily_tool = TavilySearch(
+    tavily_api_key=settings.TAVILY_API_KEY,
+    max_results=2,
+    #include_raw_content=True
+)
+@tool
+def search_law(query: str) -> str:
+    """Busca información sobre una ley en internet y devuelve un resumen en lenguaje sencillo. Usa esta herramienta cuando la respuesta devuelta por la base de datos incluya una ley"""
+    results = tavily_tool.invoke(query)
+    if results and len(results) > 0:
+        return results[0]['content']  # o el campo que contenga el texto
+    return "No se encontró información sobre la ley solicitada."
+
+
+
+tools = [neo4j_query, search_law]
 
 # brain that decides what to do and when to use each tool (but not executes them)
-agent_runnable = create_tool_calling_agent(
-    llm,
-    tools,
-    AGENT_PROMPT,
-    #handle_parsing_errors=True,  # force agent to use tools
+AGENT = create_agent(
+    model = llm,
+    tools = tools,
+    system_prompt = SYSTEM_AGENT_PROMPT,
 )
 
-# engine that really call tools and manage the conversation
-AGENT = AgentExecutor(agent=agent_runnable, tools=tools, verbose=True, return_intermediate_steps=True)
+# for message_chunk, metadata in AGENT.stream(
+#     {"messages": [{"role": "user", "content": "Que es un certificado IRPF?"}]},
+#     stream_mode="messages"  # o "messages" para tokens individuales
+# ):
+#     if hasattr(message_chunk, "content"):
+#         content = message_chunk.content
+#     else:
+#         content = None
+
+#     response_metadata = getattr(message_chunk, "response_metadata", {})
+#     usage_metadata = getattr(message_chunk, "usage_metadata", {})
+
+#     # Mostrar información del chunk
+#     #print("\n--- AGENT STREAM CHUNK ---")
+#     print(f"Contenido: {repr(content)}")
+#     #print(f"Response metadata: {response_metadata}")
+#     #print(f"Metadata interno: {metadata}")
+#     #print(f"Uso de tokens: {usage_metadata}")
+
+
+for chunk in AGENT.stream(
+    {"messages": [{"role": "user", "content": "tu pregunta"}]},
+    stream_mode="updates"
+):
+    for node, data in chunk.items():
+        if "messages" in data:
+            for msg in data["messages"]:
+                if hasattr(msg, "tool_calls") and msg.tool_calls:
+                    for tool_call in msg.tool_calls:
+                        print(f"Tool usada: {tool_call['name']}")
--- etl_load/src/node_embedder.py
+++ etl_load/src/node_embedder.py
@@ -14,7 +14,7 @@ class NodeEmbedder:
         self.db = Neo4jLoader(Neo4jConfig())
         self.embedding_generator = OpenAIEmbeddingsGenerator()
 
-    def compute_embeddings_for_procedure_node(self):
+    def compute_embeddings_for_procedure_nodes(self):
         try: 
             self.db.connect()
             logging.info("Computing embeddings for procedures...")
@@ -27,16 +27,15 @@ class NodeEmbedder:
                 coalesce(a.nombre, '')      AS area,
                 coalesce(c.nombre, '')      AS categoria,
                 coalesce(p.nombre, '')      AS nombre,
-                coalesce(p.descripcion, '') AS descripcion
+                coalesce(p.content, '')      AS descripcion
+
             WITH p,
                 'Área: ' + area + '\n' +
                 'Categoría: ' + categoria + '\n' +
                 'Procedimiento: ' + nombre + '\n' +
                 'Descripción del procedimiento: ' + descripcion AS texto
             RETURN elementId(p) AS eid,
-                labels(p)    AS labels,
-                properties(p) AS props,
-                texto        AS embedding_text
+                texto  AS text_to_embed
             """
 
             results = self.db.run_read(query)
@@ -44,59 +43,26 @@ class NodeEmbedder:
             for r in results:
                 eid = r.get("eid")
                 logging.info(f"---TEXTO FOR EMBEDDINGS---: {r}")
-                # clean_text = self._clean_text(r)
-                # logger.info(f"---{clean_text}---")
-                # vector = self.embedding_generator.embed_query(clean_text)
-                # self.db.run_write(
-                #     """
-                #     MATCH (n) WHERE elementId(n) = $id
-                #     SET n.embedding = $vector
-                #     """,
-                #     parameters = {
-                #     "id": eid, 
-                #     "vector": vector
-                # }
-                # )
+                text_to_embed = r.get("text_to_embed")
+
+                vector = self.embedding_generator.embed_query(text_to_embed)
+                self.db.run_write(
+                    """
+                    MATCH (p:Procedimiento)
+                    WHERE elementId(p) = $id
+                    SET p.embedding = $vector
+                    """,
+                    parameters = {
+                    "id": eid, 
+                    "vector": vector
+                }
+                )
 
 
             self.db.close()
         except Exception as e:
             logging.error(f"Error computing embeddings for nodes: {e}")
 
-    def _clean_text(self, res:Dict):
-        """
-        This method cleans the result of an input query to avoid incluiding
-        in the embeddings generation embeddings 
-        """
-        #remove CommonLabel from labels
-        clean_labels = [l for l in res.get("labels") if l != "CommonLabel"]
-        label_text = " ".join(clean_labels)
-
-        #extract properties
-        props = res.get("props", {})
-        nombre = props.get("nombre", "").strip() if isinstance(props.get("nombre"), str) else ""
-        content = props.get("content")
-
-        if isinstance(content, list):
-            content = " ,".join(str(x).strip() for x in content if str(x).strip())
-        
-        elif isinstance(content, (int, float, bool)):
-            content = str(content).strip()
-
-        elif isinstance(content, str):
-            content = str(content).strip()
-        
-        #join labels and properties on a unified text
-        if nombre and content:
-            clean_result = f"{label_text}: {nombre} - {content}"
-        elif nombre:
-            clean_result = f"{label_text}: {nombre}"
-        elif content:
-            clean_result = f"{label_text}: {content}"
-        else:
-            clean_result = label_text  
-        return clean_result
-    
     
     def create_vector_index(self):
         """
@@ -122,8 +88,8 @@ class NodeEmbedder:
             logging.error(f"Error creating vector index: {e}")
 
 
-node_embbeder = NodeEmbedder()
-node_embbeder.compute_embeddings_for_all_nodes()
+# node_embbeder = NodeEmbedder()
+# node_embbeder.compute_embeddings_for_procedure_node()
 
 
         
